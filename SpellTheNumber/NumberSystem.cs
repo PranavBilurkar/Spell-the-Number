f46956f26f0092bcb39c369784e0c0e4c38e75e4
unknown <pranavbilurkar28@gmail.com>
2020-05-30 20:19:04 +0530

    added NumberSystem file

---
+++ SpellTheNumber/NumberSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maersk
+{
+    static class NumberSystem
+    {
+       public const long Hundred = 100;
+       public const long Thousand = Hundred * 10;
+       public const long Million = Thousand * 1000;
+       public const long Billion = Million * 1000;
+       public const long Trillion = Billion * 1000;
+       public const long Quadrillion = Trillion * 1000;         
+    }
+}
