bcb5bdb6ad6c008158e557e48a1cab3d729fec96
unknown <pranavbilurkar28@gmail.com>
2020-05-30 21:24:38 +0530

    added numberspelling file and checked for Negative and zero rule

---
+++ SpellTheNumber/NumberSpelling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maersk
+{
+   public static class NumberSpelling
+    {
+      
+        public static string ToSpell(this long value)
+        {
+            
+            #region Negative and zero rule
+            if (value == 0) return "zero";
+
+            if (value < 0)
+            {
+                return "negative value not allowed and should be whole number";
+            } 
+            #endregion
+
+            //String Builder
+            System.Text.StringBuilder builder = new StringBuilder();
+
+            return builder.ToString();
+        }
+    }
+}
