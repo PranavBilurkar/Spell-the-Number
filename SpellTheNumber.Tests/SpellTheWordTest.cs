b06ba75ce8c59a63754498b54bae38cb022cf449
unknown <pranavbilurkar28@gmail.com>
2020-05-31 13:22:33 +0530

    added test case for each units

--- SpellTheNumber.Tests/SpellTheWordTest.cs
+++ SpellTheNumber.Tests/SpellTheWordTest.cs
@@ -27,5 +27,33 @@ namespace SpellTheNumber.Tests
             var result = NumberSpelling.ToSpell(13456);
             Assert.AreEqual(Expected, result);
         }
+
+        [TestMethod]
+        public void Test_For_Tens_Value()
+        {
+            var result = NumberSpelling.ToSpell(77);
+            Assert.AreEqual("seventy seven", result);
+        }
+
+        [TestMethod]
+        public void Test_For_Hundreds_Value()
+        {
+            var result = NumberSpelling.ToSpell(770);
+            Assert.AreEqual("seven hundred and seventy", result);
+        }
+
+        [TestMethod]
+        public void Test_For_Thousands_Value()
+        {
+            var result = NumberSpelling.ToSpell(7707);
+            Assert.AreEqual("seven thousand seven hundred and seven", result);
+        }
+
+        [TestMethod]
+        public void Test_For_Million_Value()
+        {
+            var result = NumberSpelling.ToSpell(1113770);
+            Assert.AreEqual("one million one hundred and thirteen thousand seven hundred and seventy", result);
+        }
     }
 }
