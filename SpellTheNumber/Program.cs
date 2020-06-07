d443a070390a1aba31144cf652a1e952adf9c004
Pranav Bilurkar <pranavbilurkar28@gmail.com>
2020-06-07 17:23:33 +0530

    Updated the type

--- SpellTheNumber/Program.cs
+++ SpellTheNumber/Program.cs
@@ -14,7 +14,7 @@ namespace SpellTheNumber
             {
                 long inputNumber;
                 Console.Write("Enter number to convert in word: ");
-                inputNumber = Convert.ToInt32(Console.ReadLine());
+                inputNumber = long.Parse(Console.ReadLine());
                 string outputWord = NumberSpelling.ToSpell(inputNumber);                
                 Console.WriteLine(outputWord);
                 Console.ReadLine();
